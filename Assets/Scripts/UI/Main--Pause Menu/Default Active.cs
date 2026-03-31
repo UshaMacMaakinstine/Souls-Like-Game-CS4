4f3a6c11c5eba08b208bf5d705aea0a6e29a356b
kylerArmstrong <143137470+kylerArmstrong@users.noreply.github.com>
2026-03-31 11:07:47 -0500

    settings laid out

--- Assets/Scripts/UI/Main--Pause Menu/Default Active.cs
+++ Assets/Scripts/UI/Main--Pause Menu/Default Active.cs
@@ -4,9 +4,15 @@ public class DefaultActive : MonoBehaviour
 {
     public GameObject mainFolder;
     public GameObject settingsFolder;
+    public GameObject GameSet;
+    public GameObject AudioSet;
+    public GameObject KeybindSet;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        GameSet.SetActive(true);
+        AudioSet.SetActive(false);
+        KeybindSet.SetActive(false);
         settingsFolder.SetActive(false);
         mainFolder.SetActive(true);
     }
