0bb9b899c53cc99d3c1d11a442c0847278466e18
UshaMacMaakinstine <68819202+UshaMacMaakinstine@users.noreply.github.com>
2026-04-21 11:40:57 -0500

    sadas
    asdas

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GameData
+{
+    public int savedSens;
+    public int savedMainAudio;
+    public int savedMusicAudio;
+    public int savedSFXAudio;
+    public int savedVoiceLinesAudio;
+}
