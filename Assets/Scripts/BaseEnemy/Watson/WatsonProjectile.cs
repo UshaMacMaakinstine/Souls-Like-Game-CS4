3d20b332f20b3e58a0927b7e09562b18414d570f
UshaMacMaakinstine <68819202+UshaMacMaakinstine@users.noreply.github.com>
2026-04-24 10:53:40 -0500

    asdas
    sadsad

--- Assets/Scripts/BaseEnemy/Watson/WatsonProjectile.cs
+++ Assets/Scripts/BaseEnemy/Watson/WatsonProjectile.cs
@@ -1,20 +1,24 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class WatsonProjectile : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 50f;
     public float lifetime = 5f;
     public float damage = 10f;
+
+    private GameObject player;
 
     void Start()
     {
         Destroy(gameObject, lifetime); // Clean up
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
         // Move forward relative to how it was spawned
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
